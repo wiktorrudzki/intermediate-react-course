d99b5da1adaefcf007cd89f18519fb3d7ee1f5f2
wiktorrudzki <wiktor1rudzki@gmail.com>
2022-12-29 16:17:49 +0100

    tailwind section

--- adopt-me/src/Pet.jsx
+++ adopt-me/src/Pet.jsx
@@ -9,11 +9,11 @@ const Pet = (props) => {
   }
 
   return (
-    <Link to={`/details/${id}`} className="pet">
-      <div className="image-container">
+    <Link className="relative block" to={`/details/${id}`}>
+      <div>
         <img src={hero} alt={name} />
       </div>
-      <div className="info">
+      <div className="absolute bottom-0 left-0 w-full bg-gradient-to-tr from-white to-transparent p-2">
         <h1>{name}</h1>
         <h2>{`${animal} — ${breed} — ${location}`}</h2>
       </div>
