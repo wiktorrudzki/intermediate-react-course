0cc7b3fe9f181847ffba8c627f25a5a4d7000935
wiktorrudzki <wiktor1rudzki@gmail.com>
2023-01-03 21:47:01 +0100

    server side rendering

---
+++ advanced-react-performance/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import renderApp from "./dist/server/ServerApp.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const PORT = process.env.PORT || 3001;
+
+const html = fs
+  .readFileSync(path.resolve(__dirname, "./dist/client/index.html"))
+  .toString();
+
+const parts = html.split("not rendered");
+
+const app = express();
+app.use(
+  "/assets",
+  express.static(path.resolve(__dirname, "./dist/client/assets"))
+);
+
+app.use((req, res) => {
+  res.write(parts[0]);
+
+  const stream = renderApp(req.url, {
+    onShellReady() {
+      //if crawler
+      stream.pipe(res);
+    },
+    onShellError() {
+      //do error handling here
+    },
+    onAllReady() {
+      //if the crawler
+      //stream.pipe(res);
+
+      //last thing to write
+      res.write(parts[1]);
+      res.end();
+    },
+    onError(err) {
+      console.error(err);
+    },
+  });
+});
+
+console.log(`listening on htpp://localhost:${PORT}`);
+app.listen(PORT);
--- advanced-react-performance/src/App.jsx
+++ advanced-react-performance/src/App.jsx
@@ -1,5 +1,4 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState, lazy, Suspense } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
@@ -20,31 +19,27 @@ const App = () => {
   const adoptedPet = useState(null);
   return (
     <div>
-      <BrowserRouter>
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <QueryClientProvider client={queryClient}>
-            <Suspense
-              fallback={
-                <div className="loading-pane">
-                  <h2 className="loader">⚡</h2>
-                </div>
-              }
-            >
-              <header>
-                <Link to="/">Adopt Me!</Link>
-              </header>
-              <Routes>
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/" element={<SearchParams />} />
-              </Routes>
-            </Suspense>
-          </QueryClientProvider>
-        </AdoptedPetContext.Provider>
-      </BrowserRouter>
+      <AdoptedPetContext.Provider value={adoptedPet}>
+        <QueryClientProvider client={queryClient}>
+          <Suspense
+            fallback={
+              <div className="loading-pane">
+                <h2 className="loader">⚡</h2>
+              </div>
+            }
+          >
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </Suspense>
+        </QueryClientProvider>
+      </AdoptedPetContext.Provider>
     </div>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
